d76ea78a04cebda04b1ba050f18196d6de28059f
Wayaer <wayaer@foxmail.com>
2020-10-16 10:03:00 +0800

    create lib

---
+++ lib/universally.dart
@@ -0,0 +1,11 @@
+library universally;
+
+export 'package:shared_preferences/shared_preferences.dart';
+export 'package:permission_handler/permission_handler.dart';
+export 'package:connectivity/connectivity.dart';
+export 'package:provider/provider.dart';
+export 'package:url_launcher/url_launcher.dart';
+export 'package:webview_flutter/webview_flutter.dart';
+export 'package:dio/dio.dart';
+export 'package:flutter_waya/flutter_waya.dart';
+export 'package:flutter_curiosity/flutter_curiosity.dart';
