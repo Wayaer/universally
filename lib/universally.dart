35a2f0dfeab9b89dc40b34667e221f3540a9dbb4
Wayaer <wayaer@foxmail.com>
2020-10-16 13:26:12 +0800

    no message

--- lib/universally.dart
+++ lib/universally.dart
@@ -8,5 +8,3 @@ export 'package:url_launcher/url_launcher.dart';
 export 'package:webview_flutter/webview_flutter.dart';
 export 'package:dio/dio.dart';
 export 'package:path_provider/path_provider.dart';
-export 'package:flutter_waya/flutter_waya.dart';
-export 'package:flutter_curiosity/flutter_curiosity.dart';
