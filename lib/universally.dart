f7e35b3fce5030ef828b1b3c2975ae074dd69ea0
Wayaer <wayaer@foxmail.com>
2021-03-05 16:22:01 +0800

    no message

--- lib/universally.dart
+++ lib/universally.dart
@@ -4,7 +4,6 @@ export 'package:dio/dio.dart';
 export 'package:provider/provider.dart';
 export 'package:connectivity/connectivity.dart';
 export 'package:url_launcher/url_launcher.dart';
-export 'package:path_provider/path_provider.dart';
 export 'package:webview_flutter/webview_flutter.dart';
 export 'package:permission_handler/permission_handler.dart';
 export 'package:shared_preferences/shared_preferences.dart';
