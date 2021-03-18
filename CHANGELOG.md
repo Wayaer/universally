e4bcf5ca2dde32076953e0b5d370e47e3b522d14
Wayaer <wayaer@foxmail.com>
2021-03-18 17:43:27 +0800

    add example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+## [0.1.0]
+ * add example
+ * add flutter_inappwebview
+ * remove webview_flutter
 ## [0.0.6]
  * add doc
---
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.universally.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
