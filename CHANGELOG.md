8699c4aa632a95fcccc92d91de32c6c4742ec6da
Wayaer <wayaer@foxmail.com>
2021-03-22 11:02:11 +0800

    no message

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
+## [0.2.0]
+ * replace refresh component
 ## [0.1.0]
  * add example
+ * replace webview_flutter component
  * add flutter_inappwebview
- * remove webview_flutter
 ## [0.0.6]
  * add doc
--- lib/universally.dart
+++ lib/universally.dart
@@ -8,7 +8,7 @@ export 'package:flutter_inappwebview/flutter_inappwebview.dart';
 export 'package:url_launcher/url_launcher.dart';
 export 'package:permission_handler/permission_handler.dart';
 export 'package:shared_preferences/shared_preferences.dart';
-export 'package:pull_to_refresh/pull_to_refresh.dart';
+export 'package:flutter_easyrefresh/easy_refresh.dart';
 
 export 'package:flutter_waya/flutter_waya.dart';
 export 'package:flutter_curiosity/flutter_curiosity.dart';
