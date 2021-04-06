0ac7e9554bf9532ff1058c056c08181b365c09bb
Wayaer <wayaer@foxmail.com>
2021-04-06 12:10:12 +0800

    no message

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## [0.3.0]
+## [0.3.1]
  * update flutter_waya
  * fix bugs
 ## [0.2.3]
--- lib/universally.dart
+++ lib/universally.dart
@@ -9,6 +9,8 @@ export 'package:provider/provider.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 export 'package:url_launcher/url_launcher.dart';
 
+export 'package:flutter_easyrefresh/easy_refresh.dart';
+
 export 'package:flutter_waya/flutter_waya.dart';
 export 'package:flutter_curiosity/flutter_curiosity.dart';
 
