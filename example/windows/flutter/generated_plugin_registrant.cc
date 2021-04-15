e8cc0cee762fea0a8e1ccd20fc456de2ae2a2a8f
Wayaer <wayaer@foxmail.com>
2021-04-15 13:21:10 +0800

    no message

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -4,9 +4,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_curiosity/curiosity_plugin.h>
 #include <url_launcher_windows/url_launcher_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  CuriosityPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("CuriosityPlugin"));
   UrlLauncherPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
 }
