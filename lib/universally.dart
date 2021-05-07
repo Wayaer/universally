bd4ce92a1691aa4088d415101af388fea6a18f39
Wayaer <wayaer@foxmail.com>
2021-05-07 11:22:07 +0800

    no message

--- lib/universally.dart
+++ lib/universally.dart
@@ -1,16 +1,24 @@
 library universally;
 
+export 'package:synchronized/synchronized.dart';
 export 'package:connectivity/connectivity.dart';
 export 'package:flutter_curiosity/flutter_curiosity.dart';
 export 'package:flutter_easyrefresh/easy_refresh.dart';
 export 'package:flutter_inappwebview/flutter_inappwebview.dart';
 export 'package:flutter_svg/flutter_svg.dart';
-export 'package:flutter_waya/flutter_waya.dart';
+export 'package:flutter_waya/flutter_waya.dart' hide Lock;
 export 'package:path_provider/path_provider.dart';
 export 'package:permission_handler/permission_handler.dart';
 export 'package:provider/provider.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 export 'package:url_launcher/url_launcher.dart';
+export 'package:path/path.dart';
+export 'package:http_parser/http_parser.dart';
+export 'package:meta/meta.dart';
+export 'package:crypto/crypto.dart';
+export 'package:clock/clock.dart';
+export 'package:collection/collection.dart';
+export 'package:vector_math/vector_math.dart';
 
 export 'src/directory.dart';
 export 'src/shared_preferences.dart';
