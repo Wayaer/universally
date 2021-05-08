07148dcb0b035d9d3529abaa19ca3863d75bb1d0
Wayaer <wayaer@foxmail.com>
2021-05-08 17:12:41 +0800

    no message

--- lib/universally.dart
+++ lib/universally.dart
@@ -12,13 +12,13 @@ export 'package:permission_handler/permission_handler.dart';
 export 'package:provider/provider.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 export 'package:url_launcher/url_launcher.dart';
-export 'package:path/path.dart';
+export 'package:path/path.dart' hide context;
 export 'package:http_parser/http_parser.dart';
 export 'package:meta/meta.dart';
 export 'package:crypto/crypto.dart';
 export 'package:clock/clock.dart';
 export 'package:collection/collection.dart';
-export 'package:vector_math/vector_math.dart';
+export 'package:vector_math/vector_math.dart' hide Colors;
 
 export 'src/directory.dart';
 export 'src/shared_preferences.dart';
