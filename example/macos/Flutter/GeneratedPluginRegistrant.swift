93e778de947da6fb5998e3e7f424949d21dc5167
Wayaer <wayaer@foxmail.com>
2023-09-11 15:06:33 +0800

    no message

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
+import device_info_plus
 import flutter_curiosity
 import package_info_plus
 import path_provider_foundation
@@ -13,6 +14,7 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   CuriosityPlugin.register(with: registry.registrar(forPlugin: "CuriosityPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
---
+++ lib/dependencies/plugin/device_info.dart
@@ -0,0 +1,41 @@
+import 'package:device_info_plus/device_info_plus.dart';
+
+class DeviceInfo {
+  factory DeviceInfo() => _singleton ??= DeviceInfo._();
+
+  DeviceInfo._();
+
+  static DeviceInfo? _singleton;
+
+  final DeviceInfoPlugin _deviceInfoPlugin = DeviceInfoPlugin();
+
+  AndroidDeviceInfo? _androidInfo;
+
+  Future<AndroidDeviceInfo> get android async {
+    return _androidInfo ??= await _deviceInfoPlugin.androidInfo;
+  }
+
+  IosDeviceInfo? _iosInfo;
+
+  Future<IosDeviceInfo> get ios async {
+    return _iosInfo ??= await _deviceInfoPlugin.iosInfo;
+  }
+
+  MacOsDeviceInfo? _macOSInfo;
+
+  Future<MacOsDeviceInfo> get macOS async {
+    return _macOSInfo ??= await _deviceInfoPlugin.macOsInfo;
+  }
+
+  WindowsDeviceInfo? _windowsInfo;
+
+  Future<WindowsDeviceInfo> get windows async {
+    return _windowsInfo ??= await _deviceInfoPlugin.windowsInfo;
+  }
+
+  LinuxDeviceInfo? _linuxInfo;
+
+  Future<LinuxDeviceInfo> get linux async {
+    return _linuxInfo ??= await _deviceInfoPlugin.linuxInfo;
+  }
+}
