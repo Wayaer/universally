618f30710841ccc8c714f47ce8e3d857e2f32d50
Wayaer <Wayaer>
2025-02-13 15:25:08 +0800

    no message

--- example/build_web.sh
+++ example/build_web.sh
@@ -1,10 +1,10 @@
 rm -rf 'app/web'
 
-echo "开始获取 packages 插件资源"
+echo '开始获取 packages 插件资源'
 
 flutter packages get
 
-echo "开始构建 web"
+echo '开始构建 web'
 
 flutter build web --base-href '/universally/example/app/web/'
 
