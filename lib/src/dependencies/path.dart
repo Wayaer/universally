6fa2e9f3ec70b725858a8c95676fbc917382a0a1
Wayaer <>
2025-03-09 16:15:01 +0800

    no message

--- lib/src/dependencies/path.dart
+++ lib/src/dependencies/path.dart
@@ -5,6 +5,24 @@ extension ExtensionPathString on String {
 
   String get basename => path.basename(this);
 
+  String extension([int level = 1]) => path.extension(this, level);
+
+  List<String> pathSplit() => path.split(this);
+
+  bool equals(String path2) => path.equals(this, path2);
+
+  bool isWithin(String child) => path.isWithin(this, child);
+
+  String relative({String? from}) => path.relative(this, from: from);
+
+  String get withoutExtension => path.withoutExtension(this);
+
+  String setExtension(String extension) => path.setExtension(this, extension);
+
+  Uri get toUri => path.toUri(this);
+
+  int get hash => path.hash(this);
+
   String get dirname => path.dirname(this);
 
   String get rootPrefix => path.rootPrefix(this);
@@ -69,21 +87,16 @@ extension ExtensionPathString on String {
     String? part13,
     String? part14,
     String? part15,
-  ]) => path.absolute(
-    this,
-    part2,
-    part3,
-    part4,
-    part5,
-    part6,
-    part7,
-    part8,
-    part9,
-    part10,
-    part11,
-    part12,
-    part13,
-    part14,
-    part15,
-  );
+  ]) =>
+      path.absolute(this, part2, part3, part4, part5, part6, part7, part8, part9, part10, part11, part12, part13, part14, part15);
+}
+
+extension ExtensionPathIterableString on Iterable<String> {
+  String get joinAll => path.joinAll(this);
+}
+
+extension ExtensionPathObject on Object {
+  String get prettyUri => path.prettyUri(this);
+
+  String get fromUri => path.fromUri(this);
 }
